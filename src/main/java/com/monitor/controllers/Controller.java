9049abba62c74b465e13a0a815aa7d09edc34a7e
ysharov <sharov.yurii@otr.ru>
2023-08-25 13:59:30 +0300

    Controller class created

---
+++ src/main/java/com/monitor/controllers/Controller.java
@@ -0,0 +1,12 @@
+package com.monitor.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+}
